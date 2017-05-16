97f6fc809f1b0767ad38c7663d9db4fb22144163
Cauê Almeida <nncl@live.com>
2017-05-15 22:29:13 -0300

    Initial commit 2

---
+++ XF.AppMOB/XF.AppMOB/Model/Professor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XF.AppMOB.Model
+{
+    public class Professor
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Titulo { get; set; }
+        public string Email { get; set; }
+        public string Senha { get; set; }
+    }
+}
