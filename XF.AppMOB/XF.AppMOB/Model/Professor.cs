f1923002e9c90fe8f42c0e03ad0aeeba204ebcc3
Cauê Almeida <nncl@live.com>
2017-05-15 22:32:24 -0300

    add repository

--- XF.AppMOB/XF.AppMOB/Model/Professor.cs
+++ XF.AppMOB/XF.AppMOB/Model/Professor.cs
@@ -14,4 +14,24 @@ namespace XF.AppMOB.Model
         public string Email { get; set; }
         public string Senha { get; set; }
     }
+
+    // Toda parte de comunicacao com a base de dados fica aqui
+    public class ProfessorRepository
+    {
+        private static List<Professor> professoresSqlAzure;
+
+        public static async Task<List<Professor>> GetProfessoresSqlAzureAsync()
+        {
+            if (professoresSqlAzure != null) return professoresSqlAzure;
+
+            var httpRequest = new HttpClient();
+            var stream = await httpRequest.GetStreamAsync(
+                "");
+
+            var professorSerializer = new DataContractJsonSerializer(typeof(List<Professor>));
+            professoresSqlAzure = (List<Professor>)professorSerializer.ReadObject(stream);
+
+            return professoresSqlAzure;
+        }
+    }
 }
